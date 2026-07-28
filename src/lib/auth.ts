d7379a320575b9d1c7c201cad0df80fb96b31cd1
M4XAW <m.picault@ecole-ipssi.net>
2026-07-28 14:04:38 +0200

    feat(auth): enhance authentication setup and remove deprecated Google auth module

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,16 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@/generated/prisma/client";
+import { prisma } from "@/lib/prisma";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgres",
+        provider: "postgresql",
     }),
+    baseURL: process.env.BETTER_AUTH_URL,
+    socialProviders: {
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
 });
